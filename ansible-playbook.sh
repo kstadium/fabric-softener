17c5d9705f669393862b460cc68e8cd95842ef94
bbaktaeho <lth9036@gmail.com>
2021-08-27 14:01:51 +0900

    [TC-100] add CONTROL_NODE_PATH as env

--- ansible-playbook.sh
+++ ansible-playbook.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker run -it -v "${PWD}":/work -v ~/.ansible/roles:/root/.ansible/roles -v ~/.ssh:/root/.ssh:ro --env PY_COLORS='1' --env ANSIBLE_FORCE_COLOR='1' --rm sukill/ansible:latest ansible-playbook $@
+docker run -it -v "${PWD}":/work -v ~/.ansible/roles:/root/.ansible/roles -v ~/.ssh:/root/.ssh:ro \
+    --env PY_COLORS='1' \
+    --env CONTROL_NODE_PATH="${PWD}" \
+    --env ANSIBLE_FORCE_COLOR='1' \
+    --rm sukill/ansible:latest ansible-playbook $@
