6df3ff66af8f4a17ff322210cc9e6fb6c905c81d
bbaktaeho <lth9036@gmail.com>
2021-09-13 13:20:00 +0900

    [TC-100] generate CCP including certificate

--- ansible-playbook.sh
+++ ansible-playbook.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 docker run -it -v "${PWD}":/work -v ~/.ansible/roles:/root/.ansible/roles -v ~/.ssh:/root/.ssh:ro \
     --env PY_COLORS='1' \
-    --env CONTROL_NODE_PATH="${PWD}" \
     --env ANSIBLE_FORCE_COLOR='1' \
     --rm sukill/ansible:latest ansible-playbook $@
