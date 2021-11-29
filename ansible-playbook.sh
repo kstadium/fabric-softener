bdda0fb56964ed2506740f806044010c7923f5e0
sukill <ckdrms622@gmail.com>
2021-11-29 12:20:45 +0900

    [TD-25] remove tty option

--- ansible-playbook.sh
+++ ansible-playbook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run -it -v "${PWD}":/work -v ~/.ansible/roles:/root/.ansible/roles -v ~/.ssh:/root/.ssh:ro \
+docker run -i -v "${PWD}":/work -v ~/.ansible/roles:/root/.ansible/roles -v ~/.ssh:/root/.ssh:ro \
     --env PY_COLORS='1' \
     --env ANSIBLE_FORCE_COLOR='1' \
     --rm sukill/ansible:latest ansible-playbook $@
